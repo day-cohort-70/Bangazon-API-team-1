b47b7aeb68e5b2974ab299f14d8a352cf3c4c40f
Niki Powell <nhe.powell@gmail.com>
2024-06-25 15:31:56 -0400

    restructured

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -179,7 +179,7 @@ class Profile(ViewSet):
                     customer=current_user, payment_type=None)
                 line_items = OrderProduct.objects.filter(order=open_order)
                 line_items = LineItemSerializer(
-                     line_items, many=True, context={'request': request})
+                    line_items, many=True, context={'request': request})
 
                 cart = {}
                 cart["order"] = OrderSerializer(open_order, many=False, context={
